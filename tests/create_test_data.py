bf69b903cf00fc8f585b55a07e9cd7dc507d1403
Bernardo Aguzzoli Heberle <bag222@g.uky.edu>
2024-09-25 13:13:49 -0400

    Got CDS working

--- tests/create_test_data.py
+++ tests/create_test_data.py
@@ -1,4 +1,8 @@
+## Import libraries and set path
 import pandas as pd
+import os
+import sys
+sys.path.append("C:/Users/local_bag222/Desktop/dash_apps/AD_RNAseq_dash_app")
 
 ## Define data
 data = {
@@ -12,50 +16,63 @@ data = {
     'transcript_biotype': ['NA', 'protein_coding', 'protein_coding', 'protein_coding', 'protein_coding', 'protein_coding', 'protein_coding', 'protein_coding', 'protein_coding', 'protein_coding', 'protein_coding', 'protein_coding', 'protein_coding', 'protein_coding', 'protein_coding', 'protein_coding', 'processed_transcript', 'processed_transcript', 'processed_transcript', 'processed_transcript', 'processed_transcript', 'processed_transcript', 'protein_coding', 'protein_coding', 'protein_coding', 'protein_coding', 'protein_coding', 'protein_coding', 'protein_coding', 'protein_coding', 'protein_coding', 'protein_coding', 'protein_coding', 'protein_coding', 'protein_coding', 'protein_coding', 'protein_coding', 'processed_transcript', 'processed_transcript', 'processed_transcript', 'processed_transcript', 'processed_transcript', 'processed_transcript']
 }
 
+# Create a DataFrame from the input data
 df = pd.DataFrame(data)
+
+# Filter the DataFrame to keep only relevant rows (i.e., gene, transcript, exon, CDS types)
 df = df.loc[df["type"].isin(["gene", "transcript", "exon", "CDS"])].copy()
+
+# Initialize the 'exon_number' column as null (for future assignment)
 df['exon_number'] = pd.NA
 
+# Get the list of unique transcript names, excluding any entries labeled as 'NA'
 transcripts = df['transcript_name'].unique()
 transcripts = [t for t in transcripts if t != 'NA']
 
+# Loop over each transcript to assign exon numbers
 for transcript in transcripts:
-    # Get indices of this transcript
+    # Get the indices corresponding to the current transcript in the DataFrame
     indices_transcript = df[df['transcript_name'] == transcript].index
     
-    # Get exons
+    # Extract exon rows for the current transcript
     df_exons = df.loc[indices_transcript]
     df_exons = df_exons[df_exons['type'] == 'exon']
-    # Sort exons by start position
+    
+    # Sort exons by their start position (assuming exon numbers are ordered by genomic location)
     df_exons = df_exons.sort_values(by='start')
-    # Assign exon numbers starting from 1
+    
+    # Create a list of exon numbers starting from 1, matching the number of exons
     exon_numbers = list(range(1, len(df_exons) + 1))
+    
+    # Assign the calculated exon numbers to the 'exon_number' column of exon rows
     df_exons = df_exons.assign(exon_number=exon_numbers)
-    # Update exon_number in the main dataframe for exons
+    
+    # Update the main DataFrame with the assigned exon numbers for the exons
     df.loc[df_exons.index, 'exon_number'] = exon_numbers
     
-    # Map from exon start/end to exon number
+    # Create a list of tuples containing the start, end, and exon number for the current transcript
     exon_coords = list(zip(df_exons['start'], df_exons['end'], exon_numbers))
     
-    # Now, for each row in df_transcript, assign exon_number if applicable
+    # Iterate over each row of the current transcript to assign exon numbers where applicable
     for index in indices_transcript:
         row = df.loc[index]
+        
+        # For specific row types (CDS, start/stop codon, UTRs), try to assign an exon number
         if row['type'] in ['CDS', 'start_codon', 'stop_codon', 'five_prime_utr', 'three_prime_utr']:
-            # Find the exon where the row's start position is within exon start and end
             assigned = False
+            # Assign the exon number if the row's 'start' position falls within an exon range
             for exon_start, exon_end, exon_num in exon_coords:
                 if exon_start <= row['start'] <= exon_end:
                     df.loc[index, 'exon_number'] = exon_num
                     assigned = True
                     break
+            # If no exon matches the row's position, set 'exon_number' to NA
             if not assigned:
                 df.loc[index, 'exon_number'] = pd.NA
+        
+        # For transcript rows, we explicitly set 'exon_number' to NA (not applicable)
         elif row['type'] == 'transcript':
-            # Set exon_number to NA
             df.loc[index, 'exon_number'] = pd.NA
 
-# Display the final DataFrame
-print(df)
-
-## Output test data
-df.to_csv("../raw_data/test_data/test_annotations.tsv", index=False, sep="\t")
+# Output the updated DataFrame to a test file in TSV format
+df.to_csv("./raw_data/test_data/test_annotations.tsv", index=False, sep="\t")
--- tests/ggtranscripts_test.py
+++ tests/ggtranscripts_test.py
@@ -8,20 +8,8 @@ import os
 sys.path.append("C:/Users/local_bag222/Desktop/dash_apps/AD_RNAseq_dash_app")
 from plotly_ggtranscript import geom_range, geom_intron, to_intron, shorten_gaps, set_axis, calculate_cds_exon_difference, rescale_cds
 
-# Create the sod1_annotation DataFrame
-data = {
-    'seqnames': ['21'] * 43,
-    'start': [31659666, 31659666, 31659666, 31659770, 31659770, 31663790, 31663790, 31666449, 31666449, 31667258, 31667258, 31668471, 31668471, 31668576, 31659666, 31668579, 31659693, 31659693, 31661549, 31663790, 31666449, 31667258, 31659693, 31659693, 31659770, 31659770, 31663790, 31663790, 31666449, 31666449, 31667258, 31667258, 31668471, 31668471, 31668576, 31659693, 31668579, 31659709, 31659709, 31663790, 31666449, 31667258, 31668471],
-    'end': [31668931, 31668931, 31659784, 31659784, 31659772, 31663886, 31663886, 31666518, 31666518, 31667375, 31667375, 31668931, 31668575, 31668578, 31659769, 31668931, 31667341, 31659841, 31661734, 31663886, 31666518, 31667341, 31668931, 31659841, 31659841, 31659772, 31663886, 31663886, 31666518, 31666518, 31667375, 31667375, 31668931, 31668575, 31668578, 31659769, 31668931, 31668931, 31660708, 31663886, 31666518, 31667375, 31668931],
-    'strand': ['+'] * 43,
-    'type': ['gene', 'transcript', 'exon', 'CDS', 'start_codon', 'exon', 'CDS', 'exon', 'CDS', 'exon', 'CDS', 'exon', 'CDS', 'stop_codon', 'five_prime_utr', 'three_prime_utr', 'transcript', 'exon', 'exon', 'exon', 'exon', 'exon', 'transcript', 'exon', 'CDS', 'start_codon', 'exon', 'CDS', 'exon', 'CDS', 'exon', 'CDS', 'exon', 'CDS', 'stop_codon', 'five_prime_utr', 'three_prime_utr', 'transcript', 'exon', 'exon', 'exon', 'exon', 'exon'],
-    'gene_name': ['SOD1'] * 43,
-    'transcript_name': ['NA', 'SOD1-202', 'SOD1-202', 'SOD1-202', 'SOD1-202', 'SOD1-202', 'SOD1-202', 'SOD1-202', 'SOD1-202', 'SOD1-202', 'SOD1-202', 'SOD1-202', 'SOD1-202', 'SOD1-202', 'SOD1-202', 'SOD1-202', 'SOD1-204', 'SOD1-204', 'SOD1-204', 'SOD1-204', 'SOD1-204', 'SOD1-204', 'SOD1-201', 'SOD1-201', 'SOD1-201', 'SOD1-201', 'SOD1-201', 'SOD1-201', 'SOD1-201', 'SOD1-201', 'SOD1-201', 'SOD1-201', 'SOD1-201', 'SOD1-201', 'SOD1-201', 'SOD1-201', 'SOD1-201', 'SOD1-203', 'SOD1-203', 'SOD1-203', 'SOD1-203', 'SOD1-203', 'SOD1-203'],
-    'transcript_biotype': ['NA', 'protein_coding', 'protein_coding', 'protein_coding', 'protein_coding', 'protein_coding', 'protein_coding', 'protein_coding', 'protein_coding', 'protein_coding', 'protein_coding', 'protein_coding', 'protein_coding', 'protein_coding', 'protein_coding', 'protein_coding', 'processed_transcript', 'processed_transcript', 'processed_transcript', 'processed_transcript', 'processed_transcript', 'processed_transcript', 'protein_coding', 'protein_coding', 'protein_coding', 'protein_coding', 'protein_coding', 'protein_coding', 'protein_coding', 'protein_coding', 'protein_coding', 'protein_coding', 'protein_coding', 'protein_coding', 'protein_coding', 'protein_coding', 'protein_coding', 'processed_transcript', 'processed_transcript', 'processed_transcript', 'processed_transcript', 'processed_transcript', 'processed_transcript']
-}
-
-sod1_annotation = pd.DataFrame(data)
-sod1_annotation.dropna(inplace=True)
+## Import annotations
+sod1_annotation = pd.read_csv("./raw_data/test_data/test_annotations.tsv", sep="\t")
 
 # Define a mapping from transcript_biotype to colors
 biotype_colors = {
@@ -57,70 +45,72 @@ sod1_rescaled_introns = sod1_rescaled.loc[sod1_rescaled["type"] == "intron"].cop
 sod1_rescaled_cds = rescale_cds(sod1_cds_diff, sod1_rescaled_exons)
 
 # Create the plot
-# fig = go.Figure()
+fig = go.Figure()
 
 ## Debug
-print(sod1_cds_diff.head())
-print(sod1_rescaled_cds.loc[sod1_rescaled_cds["transcript_name"] == "SOD1-202"].head(10))
-print(sod1_rescaled_exons.loc[sod1_rescaled_exons["transcript_name"] == "SOD1-202"].head(10))
+print(sod1_rescaled_cds.loc[sod1_rescaled_cds["transcript_name"].isin(["SOD1-202", "SOD1-201"])].head(20))
+print(sod1_rescaled_exons.loc[sod1_rescaled_exons["transcript_name"].isin(["SOD1-202", "SOD1-201"])].head(20))
 
-# # Add exons using geom_range, passing the fillcolor directly
-# exon_traces = geom_range(
-#     data=sod1_rescaled_exons,
-#     x_start='start',
-#     x_end='end',
-#     y='transcript_name',
-#     fill=sod1_rescaled_exons['fillcolor'], 
-#     height=0.5
-# )
+sod1_rescaled_cds.sort_values(by=["transcript_name"], inplace=True)
+sod1_rescaled_exons.sort_values(by=["transcript_name"], inplace=True)
 
-# ## Add CDS traces
-# cds_traces = geom_range(
-#     data=sod1_rescaled_cds,
-#     x_start='start',
-#     x_end='end',
-#     y='transcript_name',
-#     fill=sod1_rescaled_cds['fillcolor'],
-#     height= 0.25
-# )
+# Add exons using geom_range, passing the fillcolor directly
+exon_traces = geom_range(
+    data=sod1_rescaled_exons,
+    x_start='start',
+    x_end='end',
+    y='transcript_name',
+    fill=sod1_rescaled_exons['fillcolor'], 
+    height=0.5
+)
+
+## Add CDS traces
+cds_traces = geom_range(
+    data=sod1_rescaled_cds,
+    x_start='start',
+    x_end='end',
+    y='transcript_name',
+    fill=sod1_rescaled_cds['fillcolor'],
+    height= 0.75
+)
 
 
-# # Create introns and add them using geom_intron
-# #sod1_introns = to_intron(sod1_exons, group_var="transcript_name")
-# intron_traces = geom_intron(
-#     data=sod1_rescaled_introns,
-#     x_start='start',
-#     x_end='end',
-#     y='transcript_name',
-#     strand='strand'
-# )
+# Create introns and add them using geom_intron
+#sod1_introns = to_intron(sod1_exons, group_var="transcript_name")
+intron_traces = geom_intron(
+    data=sod1_rescaled_introns,
+    x_start='start',
+    x_end='end',
+    y='transcript_name',
+    strand='strand'
+)
 
-# # Add exons, CDS, and introns as before
-# for trace in exon_traces:
-#     fig.add_shape(trace)
+# Add exons, CDS, and introns as before
+for trace in exon_traces:
+    fig.add_shape(trace)
 
-# for trace in cds_traces:
-#     fig.add_shape(trace)
+for trace in cds_traces:
+    fig.add_shape(trace)
 
-# for trace in intron_traces:
-#     if isinstance(trace, dict):
-#         fig.add_shape(trace)
-#     else:
-#         fig.add_trace(trace)
+for trace in intron_traces:
+    if isinstance(trace, dict):
+        fig.add_shape(trace)
+    else:
+        fig.add_trace(trace)
 
-# # Call the new function to set the genomic axis range
-# fig = set_axis(fig, sod1_rescaled_exons, sod1_rescaled_introns)
+# Call the new function to set the genomic axis range
+fig = set_axis(fig, sod1_rescaled_exons, sod1_rescaled_introns)
 
-# # Update layout and show the plot
-# fig.update_layout(
-#     title="SOD1 Transcript Structure",
-#     xaxis_title="Genomic Position",
-#     yaxis_title="Transcript",
-#     height=400,
-#     width=800,
-#     showlegend=False
-# )
+# Update layout and show the plot
+fig.update_layout(
+    title="SOD1 Transcript Structure",
+    xaxis_title="Genomic Position",
+    yaxis_title="Transcript",
+    height=400,
+    width=800,
+    showlegend=False
+)
 
-# # Show or save the plot
-# fig.show()
-# fig.write_html("sod1_transcript_structure.html")
+# Show or save the plot
+fig.show()
+fig.write_html("sod1_transcript_structure.html")
